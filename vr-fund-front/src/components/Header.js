c5d6dd4362ae55039b949e2105bec58f7a89dad5
Mike Harley <mharley12345@gmail.com>
2019-09-20 16:52:09 -0700

    init setup

---
+++ vr-fund-front/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+text-align:center;
+font-size:40px;
+`
+const Header = (props)=> {
+  return (
+
+ <StyledHeader />
+  )
+}
+
+export  {StyledHeader}
