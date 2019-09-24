e5814c2e3216778e04c098c064ff46cb1cdbdd7f
Chris Boydstun <chrisboydstun@gmail.com>
2019-09-24 16:38:30 -0500

    working

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error(err.message);
-        //exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
-module.exports = connectDB; 
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
