258999200cdf796460d32dd522f573db97f3e3ba
Mike Harley <info@mike-harley.tech>
2019-09-27 10:25:15 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,62 @@
-const mongoose = require('mongoose');
-const config = require('config');
-
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
-
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
+{
+  "users": [
+    {
+      "id": 1,
+      "name": "Hort Andrin",
+      "email": "handrin0@sciencedirect.com",
+      "location": "86775 Garrison Way",
+      "projects": "Mr. Deeds Goes to Town",
+      "website": "234.67.191.111/20",
+      "type_id": 2
+    },
+    {
+      "id": 2,
+      "name": "Verina Luberti",
+      "email": "vluberti1@alibaba.com",
+      "location": "93 Arrowood Lane",
+      "projects": "Magic Man",
+      "website": "252.77.232.219/5",
+      "type_id": 1
+    },
+    {
+      "id": 3,
+      "name": "Jaye Lowrance",
+      "email": "jlowrance2@ovh.net",
+      "location": "086 Mosinee Crossing",
+      "projects": "Tyler Perry's Meet the Browns",
+      "website": "129.65.76.66/24",
+      "type_id": 2
+    },
+    {
+      "id": 4,
+      "name": "Clair Dallywater",
+      "email": "cdallywater3@addthis.com",
+      "location": "2408 Clemons Circle",
+      "projects": "Malice",
+      "website": "35.103.107.238/19",
+      "type_id": 1
+    },
+    {
+      "id": 5,
+      "name": "Bastien Whitney",
+      "email": "bwhitney4@tinyurl.com",
+      "location": "3 Manufacturers Crossing",
+      "projects": "Stolen Life, A",
+      "website": "43.249.228.224/31",
+      "type_id": 1
+    },
+    {
+      "id": 6,
+      "name": "Latashia Careless",
+      "email": "lcareless5@amazonaws.com",
+      "location": "68 Park Meadow Plaza",
+      "projects": "The Heart of the World",
+      "website": "118.203.143.159/12",
+      "type_id": 2
+    }
+    ],
+     "login": {
+    "email": "",
+    "password": ""
   }
-};
-
-
-module.exports = connectDB;
+}
